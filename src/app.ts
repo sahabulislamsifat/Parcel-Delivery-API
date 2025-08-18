c9bb408bae078fb06fa60806a493b0f54933639d
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-08-18 21:52:05 +0600

    fix some issues

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"], // চাইলে পরে live domain যোগ করবে
+    origin: ["http://localhost:5173"],
     credentials: true,
   })
 );

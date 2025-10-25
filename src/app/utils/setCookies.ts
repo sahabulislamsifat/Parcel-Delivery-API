f4033deabe3172928cc33065cad35814c1fc30c1
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-10-26 00:22:45 +0600

    issues reposlved

--- src/app/utils/setCookies.ts
+++ src/app/utils/setCookies.ts
@@ -6,25 +6,47 @@ export interface AuthToken {
   refreshToken?: string;
 }
 
-export const setAuthCookie = (res: Response, tokenInfo: AuthToken) => {
-  // const isProd = envVariables.NODE_ENV === "production";
+// export const setAuthCookie = (res: Response, tokenInfo: AuthToken) => {
+//   // const isProd = envVariables.NODE_ENV === "production";
 
+//   if (tokenInfo.accessToken) {
+//     res.cookie("accessToken", tokenInfo.accessToken, {
+//       httpOnly: true,
+//       // secure: isProd,
+//       secure: true,
+//       // sameSite: "strict",
+//       sameSite: "none",
+//     });
+//   }
+//   if (tokenInfo.refreshToken) {
+//     res.cookie("refreshToken", tokenInfo.refreshToken, {
+//       httpOnly: true,
+//       // secure: isProd,
+//       secure: true,
+//       // sameSite: "strict",
+//       sameSite: "none",
+//     });
+//   }
+// };
+
+export const setAuthCookie = (res: Response, tokenInfo: AuthToken) => {
   if (tokenInfo.accessToken) {
     res.cookie("accessToken", tokenInfo.accessToken, {
       httpOnly: true,
-      // secure: isProd,
-      secure: true,
-      // sameSite: "strict",
-      sameSite: "none",
+      secure: true, // HTTPS only
+      sameSite: "none", // allow cross-site
+      path: "/",
+      maxAge: 1000 * 60 * 15, // 15 min
     });
   }
+
   if (tokenInfo.refreshToken) {
     res.cookie("refreshToken", tokenInfo.refreshToken, {
       httpOnly: true,
-      // secure: isProd,
       secure: true,
-      // sameSite: "strict",
       sameSite: "none",
+      path: "/",
+      maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
     });
   }
 };

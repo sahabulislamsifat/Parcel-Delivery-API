ec0f8d937ca65c35034ce8e25c508a27223d0748
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-10-28 12:00:53 +0600

    get back previous

--- src/app/utils/setCookies.ts
+++ src/app/utils/setCookies.ts
@@ -6,25 +6,19 @@ export interface AuthToken {
 }
 
 export const setAuthCookie = (res: Response, tokenInfo: AuthToken) => {
-  const isDev = process.env.NODE_ENV !== "production";
-
   if (tokenInfo.accessToken) {
     res.cookie("accessToken", tokenInfo.accessToken, {
       httpOnly: true,
-      // sameSite: "none", // allow cross-site
-      // secure: true, // HTTPS only
-      sameSite: isDev ? "lax" : "none",
-      secure: !isDev, // secure only in production
+      sameSite: "none",
+      secure: true,
     });
   }
 
   if (tokenInfo.refreshToken) {
     res.cookie("refreshToken", tokenInfo.refreshToken, {
       httpOnly: true,
-      // sameSite: "none",
-      // secure: true,
-      sameSite: isDev ? "lax" : "none",
-      secure: !isDev,
+      sameSite: "none",
+      secure: true,
     });
   }
 };
